cff27e63092b957a7fd6d90736d05f6156603a2f
nawazshareefshaiksfdc <nawazshareefshaiksfdc@gmail.com>
2024-08-07 10:11:54 +0530

    initial commit

---
+++ static/script.js
@@ -0,0 +1,6 @@
+function changeLanguage() {
+    const language = document.getElementById('language').value;
+    alert('Language selected: ' + language);
+    // Add your language change logic here
+    // For example, you can use window.location to redirect to a different URL based on the selected language
+}
